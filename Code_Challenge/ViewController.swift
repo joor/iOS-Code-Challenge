0a2aee098272c490cf1e152fdac329651c9998a0
Austin Feight <afeight@umich.edu>
2018-11-25 19:40:48 -0500

    Initial Commit

---
+++ Code_Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code_Challenge
+//
+//  Created by Austin Feight on 11/25/18.
+//  Copyright © 2018 JOOR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ Code_ChallengeTests/Code_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Code_ChallengeTests.swift
+//  Code_ChallengeTests
+//
+//  Created by Austin Feight on 11/25/18.
+//  Copyright © 2018 JOOR. All rights reserved.
+//
+
+import XCTest
+@testable import Code_Challenge
+
+class Code_ChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
